70c6e8c6878f00da123c86b648687c6174cdd8de
Nathan Osti <nathanosti.job@gmail.com>
2022-06-24 20:24:51 -0300

    ✨ Create converCsv script

---
+++ backend/src/utils/convertCsv.ts
@@ -0,0 +1,41 @@
+import { Readable } from "stream";
+import readLine from "readline";
+
+import { covidCasesProps } from "src/@types/covidCasesTypes";
+import { converCsvProps } from "src/@types/convertCsvTypes";
+
+export default async function converCsv({ buffer }: converCsvProps) {
+  const readableFile = new Readable();
+
+  const bufferToString = (buffer: ArrayBuffer) => {
+    const bytes = new Uint8Array(buffer);
+    return bytes.reduce(
+      (string, byte) => string + String.fromCharCode(byte),
+      ""
+    );
+  };
+
+  readableFile.push(bufferToString(buffer));
+  readableFile.push(null);
+
+  const productLine = readLine.createInterface({
+    input: readableFile,
+  });
+
+  const covidCases: covidCasesProps[] = [];
+
+  for await (let line of productLine) {
+    const convertLineArr = line.split(",");
+
+    covidCases.push({
+      location: convertLineArr[0],
+      date: convertLineArr[1],
+      variant: convertLineArr[2],
+      num_sequences: Number(convertLineArr[3]),
+      perc_sequences: Number(convertLineArr[4]),
+      num_sequences_total: Number(convertLineArr[5]),
+    });
+  }
+
+  return covidCases;
+}
