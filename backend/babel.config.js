c5f26e370067485b34314050c7f1dd8979fdc947
Nathan Osti <nathanosti.job@gmail.com>
2022-06-24 16:05:06 -0300

    ✨ Add babel to build

---
+++ backend/babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@controllers": "./src/controllers",
+          "@models": "./src/models",
+          "@types": "./src/types",
+        },
+      },
+    ],
+  ],
+  ignore: ["**/*.spec.ts"],
+};
